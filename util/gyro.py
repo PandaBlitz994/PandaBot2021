21e18b08ca207335a738874432494cb6af50308b
Nadav <pandablitz.994@gmail.com>
2021-05-10 17:50:21 +0300

    gyro turn update

--- util/gyro.py
+++ util/gyro.py
@@ -3,8 +3,11 @@ import time
 
 
 def gyro_turn(degrees, vel, kp):
+    startPos = Robot.gyro.angle()
     while Robot.gyro.angle() != degrees:
-        pass
+        error = Robot.gyro.angle() - (degrees + startPos)
+        Robot.chassis.drive(vel, error*kp)
+    Robot.chassis.stop()
 
 def gyro_follow(time, vel, kp, target):
     start_time = time.time()
