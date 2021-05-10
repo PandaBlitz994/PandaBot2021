83f43a0867a7b217304b9207b84fc02d7a20387c
Nadav <pandablitz.994@gmail.com>
2021-05-10 19:12:25 +0300

    run 2 update

--- runs/run2.py
+++ runs/run2.py
@@ -4,8 +4,10 @@ from util.line_follow import *
 
 
 def start():
+    Robot.gyro.reset_angle(0)
     light_avg = light_follow.light_reset()
-    gyro.gyro_follow(10, 50, 2)
+    gyro.gyro_turn(90, 10, 0.2)
+    gyro.gyro_follow(20, 30, 0.2, 90)
     
 
 
